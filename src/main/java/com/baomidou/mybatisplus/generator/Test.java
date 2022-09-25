eb3a3d3da3d29c0401aa374669928f5107df2a9a
wc <2019012617@bistu.edu.cn>
2022-09-26 05:19:08 +0800

    first commit

---
+++ src/main/java/com/baomidou/mybatisplus/generator/Test.java
@@ -0,0 +1,8 @@
+package com.baomidou.mybatisplus.generator;
+
+public class Test {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("xxx","xxxxxx","xxx")
+                .execute();
+    }
+}
